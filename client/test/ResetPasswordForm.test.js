c3bacf2e0b61178d28191b62912427dff633dbf9
ZhenHern <zhenhern123@gmail.com>
2023-04-11 22:32:09 +0800

    Done ResetPasswordForm unit test

---
+++ client/test/ResetPasswordForm.test.js
@@ -0,0 +1,17 @@
+import { describe, expect, test} from 'vitest'
+import { mount } from '@vue/test-utils'
+import ResetPasswordForm from "../src/components/loginPage/ResetPasswordForm.vue"
+
+describe("Reset Password Form Method Tests", () => {
+    test("Check email validity", () => {
+        const resetPasswordWrapper = mount(ResetPasswordForm)
+        var email = "test@outlook"
+
+        resetPasswordWrapper.vm.checkEmail(email)
+        expect(resetPasswordWrapper.vm.emailValidity).toBe(false)
+
+        email = "test@outlook.com"
+        resetPasswordWrapper.vm.checkEmail(email)
+        expect(resetPasswordWrapper.vm.emailValidity).toBe(true)
+    })
+})

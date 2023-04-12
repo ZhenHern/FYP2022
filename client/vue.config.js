b08f5e07643cecd8f79dfb9fc9db08e6c98aa9d2
ZhenHern <zhenhern123@gmail.com>
2023-04-13 04:36:28 +0800

    Changed index directory

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,54 +1,55 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/FYP2022/',
-  indexPath: 'public/index.html',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/FYP2020/'
+    : '/',
   pages: {
     index: {
       entry: 'src/pages/LandingPage/main.js',
-      template: 'public/index.html',
+      template: 'index.html',
       title: 'Landing Page',
       chunks: ['index', 'landingpage']
     },
     login: {
       entry: 'src/pages/LoginPage/main.js',
-      template: 'public/index.html',
+      template: 'index.html',
       title: 'Login Page',
       chunks: ['login']
     },
     products: {
       entry: 'src/pages/ProductPage/main.js',
-      template: 'public/index.html',
+      template: 'index.html',
       title: 'Product Page',
       chunks: ['products']
     },
     userProfile: {
       entry: 'src/pages/UserProfilePage/main.js',
-      template: 'public/index.html',
+      template: 'index.html',
       title: 'User Profile Page',
       chunks: ['userProfile']
     },
     dashboard: {
       entry: 'src/pages/DashboardPage/main.js',
-      template: 'public/index.html',
+      template: 'index.html',
       title: 'Dashboard',
       chunks: ['dashboard']
     },
     editProducts: {
       entry: 'src/pages/EditProductsPage/main.js',
-      template: 'public/index.html',
+      template: 'index.html',
       title: 'Edit Products',
       chunks: ['editProducts']
     },
     orders: {
       entry: 'src/pages/OrdersPage/main.js',
-      template: 'public/index.html',
+      template: 'index.html',
       title: 'Orders',
       chunks: ['orders']
     },
     aboutUs: {
       entry: 'src/pages/AboutUsPage/main.js',
-      template: 'public/index.html',
+      template: 'index.html',
       title: 'About Us',
       chunks: ['aboutUs']
     },

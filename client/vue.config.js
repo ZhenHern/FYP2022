f92792c74139cf0af078a565434d725d6f3fa8d7
ZhenHern <zhenhern123@gmail.com>
2023-04-13 04:37:46 +0800

    Correction

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/FYP2020/'
+    ? '/FYP2022/'
     : '/',
   pages: {
     index: {

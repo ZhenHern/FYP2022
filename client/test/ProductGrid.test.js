269e68ce67445dece943dd825cfd1b3873a6ba03
ZhenHern <zhenhern123@gmail.com>
2023-04-12 23:14:13 +0800

    Done ProductGrid unit test

---
+++ client/test/ProductGrid.test.js
@@ -0,0 +1,24 @@
+import { describe, expect, test} from 'vitest'
+import { mount } from '@vue/test-utils'
+import ProductGrid from "../src/components/productPage/ProductGrid.vue"
+
+describe("Reset Product Grid Method Tests", () => {
+    test("Decrease the product quantity", () => {
+        const productGridWrapper = mount(ProductGrid)
+        productGridWrapper.vm.quantity = [1, 2, 3]
+
+        expect(productGridWrapper.vm.quantity[0]).toBe(1)
+        productGridWrapper.vm.minusQuantity(0)
+        expect(productGridWrapper.vm.quantity[0]).toBe(0)
+        productGridWrapper.vm.minusQuantity(0)
+        expect(productGridWrapper.vm.quantity[0]).toBe(0)
+    })
+    test("Increase the product quantity", () => {
+        const productGridWrapper = mount(ProductGrid)
+        productGridWrapper.vm.quantity = [1, 2, 3]
+
+        expect(productGridWrapper.vm.quantity[0]).toBe(1)
+        productGridWrapper.vm.addQuantity(0)
+        expect(productGridWrapper.vm.quantity[0]).toBe(2)
+    })
+})
